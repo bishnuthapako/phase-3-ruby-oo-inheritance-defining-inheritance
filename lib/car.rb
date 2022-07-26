0388cdddbfa777d6253ce0ff39828cb4f90136f6
Bishnu <bishnuthapako@gmail.com>
2022-07-26 15:51:11 -0400

    completed

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,11 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+class Car < Vehicle
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+    def fill_up_tank
+        "filling up!"
+    end
+end
